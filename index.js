595422763954240b23ad1687788216740a4879d0
QueenTesa Fuggett <qfuggett@umich.edu>
2020-12-30 22:12:39 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+let random = Math.random() + 1;
+let num3 = 28;
+let num4 = 6;
+let mod = num3 % num4;
+let max = Math.max(20, 4, 1, 17);
